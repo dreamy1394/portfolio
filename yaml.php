5bd420aa51bd9ef0546b9c3768d12dc6ee2f8644
dreamy1394 <tanguisrael@gmail.com>
2026-08-16 17:59:12 +0200

    Delete yaml.php

--- yaml.php
+++
@@ -1,7 +0,0 @@
-services:
-  - type: web
-    name: mon-site-php
-    env: docker
-    buildCommand: docker build -t render-build .
-    startCommand: docker run -d -p 10000:80 --name render-app render-build
-    autoDeploy: true
