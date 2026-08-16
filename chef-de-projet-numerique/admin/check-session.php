dad47f2c75aba4a78dbc32d67d984f58165ef650
dreamy1394 <tanguisrael@gmail.com>
2026-08-16 17:58:01 +0200

    Delete chef-de-projet-numerique directory

--- chef-de-projet-numerique/admin/check-session.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/config.php';
-require_once __DIR__ . '/../includes/auth.php';
-
-checkSessionTimeout();
-echo json_encode(['status' => 'ok']);
--- chef-de-projet-numerique/admin/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/auth.php';
-
-logout();
-header('Location: /admin/login.php');
-exit;
